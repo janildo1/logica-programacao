ec4f53bc9353fd7fe00e05c1f6f3a5d884346828
janildo1 <janildo@outlook.com>
2022-10-14 22:49:14 -0300

    Questao04

--- src/atividade01/Questao04.java
+++ src/atividade01/Questao04.java
@@ -1,10 +1,27 @@
 package atividade01;
 
+import java.util.Scanner;
+
 public class Questao04 {
     public static void main(String[] args) {
         /*4. Escreva um programa para ler o nome, valor da hora, quantas horas trabalhadas,
         o sistema vai apresentar o salário.
         <fulano> vai receber R$<00.00> */
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite o nome do funcionário: ");
+        String nome = ler.nextLine();
+
+        System.out.println("Informe o valor da hora: ");
+        float valorHora = ler.nextFloat();
+
+        System.out.println("Informe a quantidade de horas trabalhadas: ");
+        int qtHoras = ler.nextInt();
+
+        float salario = (valorHora * qtHoras);
+        System.out.println(nome+" vai receber R$ "+salario);
+
+        ler.close();      
         
     }
     
