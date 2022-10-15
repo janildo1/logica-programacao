fe2e74df3464d8d0bb1bb08d2d06bbabf09cae33
janildo1 <janildo@outlook.com>
2022-10-14 22:21:01 -0300

    Questao03

--- src/atividade01/Questao03.java
+++ src/atividade01/Questao03.java
@@ -1,8 +1,19 @@
 package atividade01;
 
+import java.util.Scanner;
+
 public class Questao03 {
     public static void main(String[] args) {
-        /*3. Escreva um programa para ler um nome digitado e mostrar a mensagem "Olá <nome-digitado>".*/
+        /*3. Escreva um programa para ler um nome digitado e 
+        mostrar a mensagem "Olá <nome-digitado>".*/
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = ler.nextLine();
+        ler.close();
+
+        System.out.println("Olá, Seja bem-vindo Senhor(a) "+nome);
+
     }
     
 }
