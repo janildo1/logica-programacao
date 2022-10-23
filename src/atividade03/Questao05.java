c90c96a41bb936eca2055caed94a66c27194f08e
janildo1 <janildo@outlook.com>
2022-10-23 11:57:04 -0300

    Atualização da lista de exercicios 03

--- src/atividade03/Questao05.java
+++ src/atividade03/Questao05.java
@@ -1,9 +1,41 @@
 package atividade03;
 
+import java.util.Scanner;
+
 public class Questao05 {
     public static void main(String[] args) {
         /*5. Escreva um programa que vai ler a idade de 5 pessoas,
         o sistema vai mostrar quantas são maior ou iguais a 18 e quantas são de menor. */
+        
+        Scanner ler = new Scanner(System.in);
+
+        int idade, cont = 0, maior = 0, menor = 0;
+        
+        // Lendo a idade de 5 pessoas
+        do{
+            System.out.println("Digite uma idade: ");
+            idade = ler.nextInt();
+
+            // Idade maior ou igual a 18 anos
+            if(idade >= 18){
+                maior++; // contar a quantidade de pessoas
+            }
+            
+            // Idade menor que 18 anos
+            if(idade < 18){
+                menor++;// contar a quantidade de pessoas
+            }
+
+            cont++;
+            System.out.println("\n");
+        }while(cont < 5);
+
+        // Quantidade de pessoas com idade maior ou igual a 18 anos
+        System.out.println("Quantidade de pessoas com idade igual ou superior a 18 anos: " + maior + "\n");
+
+        // Quantidade de pessoas com idade inferior a 18 anos
+        System.out.println("Quantidade de pessoas com idade inferior a 18 anos: " + menor + "\n");     
+
     }
     
 }
--- src/atividade03/Questao06.java
+++ src/atividade03/Questao06.java
@@ -1,9 +1,48 @@
 package atividade03;
 
+import java.util.Scanner;
+
 public class Questao06 {
     public static void main(String[] args) {
         /*6. Escreva um programa que vai ler 5 números o sistema vai mostrar
          o maior o menor a soma e a média dos números digitados. */
+
+         Scanner ler = new Scanner(System.in);
+
+         int numero, totalSoma, maior = 0, menor = 1000, media = 0, soma = 0;
+         int contMaior = 0, contMenor = 0, i;
+         
+         for(i = 0; i < 5; i++){
+             System.out.println("Digite um número entre 0 e 1000: ");
+             numero = ler.nextInt();
+             
+             // Validar o intervalo entre 0 e 1000
+             if( (numero < 1) || (numero > 1000) ){
+                 i = i - 1;
+             } else{
+                 // Guardar os número digitados
+                 soma = soma + numero;
+ 
+                 // Guardar o maior número
+                 if(numero > maior){
+                     maior = numero;
+                     contMaior++;
+                 }
+ 
+                 // Guardar o menor número
+                 if(numero < menor){
+                     menor = numero;
+                     contMenor++;
+                 }   
+             
+             } // final validação
+         } 
+         //Imprimindo o maior, o menor, a soma e a média dos números digitados.
+         System.out.println("O maior número digitado foi: " + maior);
+         System.out.println("O menor número digitado foi: " + menor);
+         System.out.println("A soma dos números digitados foi: " + soma);
+         System.out.println("A média dos números digitados foi: " + (media = soma / i));
+
     }
     
 }
