dad8ac18e8395eeb3d353ef1f2425fdaa105e488
janildo1 <janildo@outlook.com>
2022-11-03 21:39:55 -0300

    Correção - exercício LPOO 01 e outros exemplos

---
+++ src/exemplos/ExemplosPoo/Carro.java
@@ -0,0 +1,28 @@
+package exemplos.ExemplosPoo;
+
+public class Carro extends Veiculo {
+
+    private int portas;
+
+    Carro(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.portas = 2;
+    }
+
+    Carro(String marca, String modelo, int portas) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.portas = portas;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro [marca=" + marca + ", modelo=" + modelo + ", velocidade=" + velocidade + ", portas=" + portas
+                + "]";
+    }
+
+    public void printStatus() {
+        System.out.println(toString());
+    }
+}
